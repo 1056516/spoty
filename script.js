82c47382655f448c0cf2bd38057860e90bdb389e
Filmzone&Flixify <159073116+1056516@users.noreply.github.com>
2024-09-26 21:17:55 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const playlistId = '7M6TVXRM77DyN6yQstAVxL';
+
+<iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src={`https://open.spotify.com/embed/playlist/7M6TVXRM77DyN6yQstAVxL?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
